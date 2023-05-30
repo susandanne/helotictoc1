c5b62a17b800c530067ffb551f151f1fa7541d38
susandanne <susandanne@gmail.com>
2023-05-31 01:21:31 +0600

    design complete

--- app/src/main/java/com/example/helotictoc1/StartActivity2.kt
+++ app/src/main/java/com/example/helotictoc1/StartActivity2.kt
@@ -1,11 +1,20 @@
 package com.example.helotictoc1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class StartActivity2 : AppCompatActivity() {
+    lateinit var button2:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start2)
+
+        button2=findViewById(R.id.button2)
+        button2.setOnClickListener {
+            var intent=Intent(this,GameActivity2::class.java)
+            startActivity(intent)
+        }
     }
 }
